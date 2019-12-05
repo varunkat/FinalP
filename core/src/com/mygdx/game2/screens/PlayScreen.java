326c1c4849954a36ec074fe4599b10c62908f2b2
varunkat <varun.sky56@gmail.com>
2019-12-04 20:40:50 -0500

    updated the camera ports

--- core/src/com/mygdx/game2/screens/PlayScreen.java
+++ core/src/com/mygdx/game2/screens/PlayScreen.java
@@ -3,15 +3,22 @@ package com.mygdx.game2.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game2.MyGdxGame;
 
 public class PlayScreen implements Screen {
 private MyGdxGame game;
 Texture texture;
+private OrthographicCamera gamecam;
+private Viewport gamePort;
     public PlayScreen(MyGdxGame game){
         this.game = game;
         texture = new Texture("badlogic.jpg");
+        gamecam = new OrthographicCamera();
+        gamePort = new StretchViewport(800, 480, gamecam);
     }
     @Override
     public void show() {
@@ -22,6 +29,7 @@ Texture texture;
     public void render(float delta) {
         Gdx.gl.glClearColor(1,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(gamecam.combined);
         game.batch.begin();
         game.batch.draw(texture, 0, 0);
         game.batch.end();
@@ -30,6 +38,7 @@ Texture texture;
 
     @Override
     public void resize(int width, int height) {
+        gamePort.update(width,height);
 
     }
 
